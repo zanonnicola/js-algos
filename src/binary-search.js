0a3f6a02309b3530d012d78fd39eed9b510f747b
zanonnicola <nicola.zanon@springernature.com>
2018-09-05 10:49:02 +0200

    Add Binary Searh algo + tests

--- src/binary-search.js
+++ src/binary-search.js
@@ -1,5 +1,31 @@
 const binarySearch = (sortedArray, element) => {
-	return -1;
+	let min = 0; // The current minimum reasonable guess for this round
+	let max = sortedArray.length - 1; // The current maximum reasonable guess.
+	let guess;
+	let i = sortedArray.length;
+
+	if (sortedArray.length === 0) {
+		return -1;
+	}
+	while (i > 0) {
+		guess = Math.floor((min + max) / 2);
+
+		// Exit conditions
+		if (max < min) {
+			return -1; // Element is not present in array
+		}
+		if (sortedArray[guess] === element) {
+			return guess;
+		}
+
+		// Logic
+		if (sortedArray[guess] < element) {
+			min = guess + 1; // The guess was too low
+		} else if (sortedArray[guess] > element) {
+			max = guess - 1; // The guess was too high
+		}
+		i--;
+	}
 };
 
 export {binarySearch};
--- src/binary-search.test.js
+++ src/binary-search.test.js
@@ -1,7 +1,17 @@
 import {binarySearch} from './binary-search';
 
 describe('Binary Search', () => {
-	it('should search number in sorted array', () => {
+	const testArray = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+	it('should return -1 for an empty array', () => {
 		expect(binarySearch([], 1)).toBe(-1);
+	});
+	it('should find the correct index if target is on the first half', () => {
+		expect(binarySearch(testArray, 13)).toBe(5);
+	});
+	it('should find the correct index if target is on the second half', () => {
+		expect(binarySearch(testArray, 73)).toBe(20);
+	});
+	it('should return -1 element is not found in array', () => {
+		expect(binarySearch(testArray, 12)).toBe(-1);
 	});
 });
